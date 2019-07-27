afabfaa923c499eb306c7ab47ed096cd6d33e82f
manish_chauhan <prince_manish@outlook.com>
2019-07-27 08:04:44 +0530

    "My First Java class commit"

---
+++ src/main/java/org/prince/javafirst/FirstJavaClass.java
@@ -0,0 +1,13 @@
+package org.prince.javafirst;
+
+public class FirstJavaClass {
+
+    public static void main(String[] args) {
+        System.out.println("\"My First Java Program!\" = " + "My First Java Program!");
+        int n =100;
+
+        for(int i=1; i<=100;i++){
+            System.out.println(i);
+        }
+    }
+}
